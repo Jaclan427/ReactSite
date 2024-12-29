94c278f988e7fa2d246b3523082753be0fc5a396
Jack Lancaster <jaclan427@gmail.com>
2024-12-29 16:09:52 -0500

    Update to transfer to MB

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,38 +3,19 @@ import './Navbar.css';
 import Logo from './Assets/Logo.png';
 
 function Navbar() {
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-
-    const handleMouseEnter = () => {
-        setDropdownOpen(true);
-    };
-
-    const handleMouseLeave = () => {
-        setDropdownOpen(false);
-    };
-
     return (
         <header className="navbar">
             <div className="navbar-logo">
                 <img src={Logo} alt="Logo" />
-                <span>EDGE</span>
+                <span>Edge</span>
             </div>
             <nav>
                 <ul className="navbar-links">
-                    <li><a href="#features">Features</a></li>
+                    <li><a href="#about">About</a></li>
                     <li><a href="#pricing">Pricing</a></li>
-                    <li
-                        className="dropdown"
-                        onMouseEnter={handleMouseEnter}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        <span>More</span>
-                        {dropdownOpen && (
-                            <div className="dropdown-menu">
-                                <a href="#Aboutus">About</a>
-                                <a href="#contact">Contact</a>
-                            </div>
-                        )}
+                    <li><a href="#contact">Contact</a></li>
+                    <li>
+                        <button className="get-started-btn">Get Started</button>
                     </li>
                 </ul>
             </nav>
